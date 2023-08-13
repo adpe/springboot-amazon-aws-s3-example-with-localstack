11bcccba8bffa5ac6f95d9a40aaabaa8936d6bd2
Adrian Perez <me@adrianperez.me>
2023-08-13 09:20:13 +0200

    Update Amazon AWS SDK and fix config
    Fixed this error: com.amazonaws.services.s3.model.AmazonS3Exception: The AWS Access Key Id you provided does not exist in our records. (Service: Amazon S3; Status Code: 403; Error Code: InvalidAccessKeyId; Request ID: <request-id>)
    
    Default the sdk accesses the cloud endpoint and not the local one, therefore we must define it.

--- src/main/java/com/s3example/demo/config/AWSConfig.java
+++ src/main/java/com/s3example/demo/config/AWSConfig.java
@@ -1,32 +1,39 @@
 package com.s3example.demo.config;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class AWSConfig {
 
-    public AWSCredentials credentials() {
-        AWSCredentials credentials = new BasicAWSCredentials(
-                "accesskey",
-                "secretkey"
-        );
-        return credentials;
-    }
+    @Autowired
+    private Environment env;
 
     @Bean
     public AmazonS3 amazonS3() {
-        AmazonS3 s3client = AmazonS3ClientBuilder
+        return AmazonS3ClientBuilder
                 .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials()))
-                .withRegion(Regions.US_EAST_1)
+                .withCredentials(getCredentialsProvider())
+                .withEndpointConfiguration(getEndpointConfiguration())
                 .build();
-        return s3client;
+    }
+
+    private AWSStaticCredentialsProvider getCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                env.getProperty("aws.accessKey", ""),
+                env.getProperty("aws.secretKey", "")));
+    }
+
+    private EndpointConfiguration getEndpointConfiguration() {
+        return new EndpointConfiguration(
+                env.getProperty("aws.serviceEndpoint", ""),
+                env.getProperty("aws.signingRegion", ""));
     }
 }
